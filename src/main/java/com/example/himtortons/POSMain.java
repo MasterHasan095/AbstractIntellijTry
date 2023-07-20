8dc5ab598231088daa1247b46fc64f611bc2eb41
masterha <masterha@sheridancollege.ca>
2023-07-20 16:57:23 -0400

    BASIC LAYOUT MADE

--- src/main/java/com/example/himtortons/POSMain.java
+++ src/main/java/com/example/himtortons/POSMain.java
@@ -3,19 +3,48 @@ package com.example.himtortons;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class POSMain extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        StackPane pane = new StackPane();
-        Button button = new Button("Nice");
-        button.setOnAction(actionEvent -> {
-            paymentMethod paymentMethod = new paymentMethod("CASH",50,25);
-        });
-        pane.getChildren().add(button);
-        Scene scene = new Scene(pane,500,500);
+        AnchorPane primaryPane = new AnchorPane();
+        ScrollPane majorMenuPane = new ScrollPane();
+        majorMenuPane.setStyle("-fx-background-color:aqua");
+        StackPane minorMenuPane = new StackPane();
+        minorMenuPane.setStyle("-fx-background-color:purple");
+        ScrollPane orderPane = new ScrollPane();
+        orderPane.setStyle("-fx-background-color:red");
+        GridPane orderGrid = new GridPane();
+        orderGrid.setStyle("-fx-background-color:green");
+        primaryPane.getChildren().addAll(majorMenuPane,minorMenuPane,orderPane,orderGrid);
+        AnchorPane.setTopAnchor(majorMenuPane, 0.0);    //
+        AnchorPane.setLeftAnchor(majorMenuPane, 0.0);
+        majorMenuPane.setPrefHeight(800);
+        majorMenuPane.setPrefWidth(300);
+        AnchorPane.setTopAnchor(orderPane,0.0);
+        AnchorPane.setRightAnchor(orderPane,0.0);
+        orderPane.setPrefHeight(400);
+        orderPane.setPrefWidth(300);
+        AnchorPane.setBottomAnchor(orderGrid,0.0);
+        AnchorPane.setRightAnchor(orderGrid,0.0);
+        orderGrid.setPrefHeight(400);
+        orderGrid.setPrefWidth(300);
+        AnchorPane.setTopAnchor(minorMenuPane,0.0);
+        AnchorPane.setLeftAnchor(minorMenuPane,300.0);
+        minorMenuPane.setPrefHeight(800);
+        minorMenuPane.setPrefWidth(800);
+
+
+
+
+
+        Scene scene = new Scene(primaryPane,1400,800);
         stage.setTitle("POS");
         stage.setScene(scene);
         stage.show();
